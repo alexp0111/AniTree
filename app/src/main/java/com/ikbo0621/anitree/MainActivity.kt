a8bab3ba21e451e6e754bcad6c701daadf593937
Alexander <alexp0111@mail.ru>
2023-04-02 16:17:40 +0300

    UI for testing

--- app/src/main/java/com/ikbo0621/anitree/MainActivity.kt
+++ app/src/main/java/com/ikbo0621/anitree/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.ikbo0621.anitree
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ikbo0621.anitree.testUI.LogInFragment
+
+
+/**
+ * Main class.
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Alex commit in backend branch
-        // Structures added
-
-        // doing some stuff in backend (test)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, LogInFragment())
+            .commitAllowingStateLoss()
     }
 }
---
+++ app/src/main/java/com/ikbo0621/anitree/testUI/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.ikbo0621.anitree.testUI
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ikbo0621.anitree.R
+
+
+class ProfileFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
