0a8f09e48e4af343020d331e9da8b851ae61f441
Alexander <alexp0111@mail.ru>
2023-04-10 16:41:38 +0300

    Parsing logic bugfix

--- app/src/main/java/com/ikbo0621/anitree/model/implementation/ParsingModel.kt
+++ app/src/main/java/com/ikbo0621/anitree/model/implementation/ParsingModel.kt
@@ -11,12 +11,17 @@ import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
 
 /**
  * Model that provides parsing logic
  * */
 class ParsingModel() : ParsingRepository {
+
+    //TODO: Guessing search
+
+    /**
+     * Get anime directly with argument
+     * */
     override suspend fun getAnimeWithName(animeTitle: String, result: (UiState<Anime>) -> Unit) {
 
         val anim: Anime = withContext(Dispatchers.IO) {
@@ -24,44 +29,55 @@ class ParsingModel() : ParsingRepository {
             /**
              * Get html page
              * */
-            val doc: Document = Jsoup
-                .connect(ParserConstants.BASIC_URL + "anime/" + animeTitle)
-                .get()
+            try {
+                val doc: Document = Jsoup
+                    .connect(ParserConstants.BASIC_URL + "anime/" + animeTitle)
+                    .get()
 
-            /**
-             * Get necessary elements by classes
-             * */
-            val pElement: Element = doc.getElementsByAttributeValue("class", "pure-1 md-3-5")[0]
-            val aElement: Element = doc.getElementsByAttributeValue("class", "pure-g entryBar")[0]
-            val imgElement: Element = doc.getElementsByAttributeValue("class", "mainEntry")[0]
+                /**
+                 * Get necessary elements by classes
+                 * */
+                val pElement: Element = doc.getElementsByAttributeValue("class", "pure-1 md-3-5")[0]
+                val aElement: Element =
+                    doc.getElementsByAttributeValue("class", "pure-g entryBar")[0]
+                val imgElement: Element = doc.getElementsByAttributeValue("class", "mainEntry")[0]
 
-            /**
-             * Get info from elements in necessary type
-             * */
-            val description: String = pElement.select("p")[0].text().toString()
-            val studio: String = aElement.select("a")[0].text().toString()
-            val releaseDate: String = aElement.select("a")[1].text().toString()
-            val imageURI: Uri = imgElement.html()
-                .substringAfter("src=\"")
-                .substringBefore("\">")
-                .toUri()
+                /**
+                 * Get info from elements in necessary type
+                 * */
+                val description: String = pElement.select("p")[0].text().toString()
+                val studio: String = aElement.select("a")[0].text().toString()
+                val releaseDate: String = aElement.select("a")[1].text().toString()
+                val imageURI: Uri = imgElement.html()
+                    .substringAfter("src=\"")
+                    .substringBefore("\">")
+                    .toUri()
 
-            /**
-             * Create Anime object
-             * */
-            val anim = Anime(
-                animeTitle,
-                description,
-                studio,
-                releaseDate,
-                imageURI
-            )
+                /**
+                 * Create Anime object
+                 * */
+                val anim = Anime(
+                    animeTitle,
+                    description,
+                    studio,
+                    releaseDate,
+                    imageURI
+                )
 
-            return@withContext anim
+                return@withContext anim
+            } catch (e: java.lang.Exception) {
+                return@withContext Anime()
+            }
         }
 
-        result.invoke(
-            UiState.Success(anim)
-        )
+        if (anim.title == "-1") {
+            result.invoke(
+                UiState.Failure("Anime not found")
+            )
+        } else {
+            result.invoke(
+                UiState.Success(anim)
+            )
+        }
     }
 }
--- app/src/main/java/com/ikbo0621/anitree/structure/Anime.kt
+++ app/src/main/java/com/ikbo0621/anitree/structure/Anime.kt
@@ -3,11 +3,11 @@ package com.ikbo0621.anitree.structure
 import android.net.Uri
 
 data class Anime(
-    var title: String,
-    var description: String,
-    var studio: String,
-    var releaseDate: String,
-    var imageURI: Uri
+    var title: String = "-1",
+    var description: String = "",
+    var studio: String = "",
+    var releaseDate: String = "",
+    var imageURI: Uri = Uri.EMPTY
 ){
     override fun toString(): String {
         return "Anime(\ntitle='$title'\n\ndescription='$description'\n\nstudio='$studio'\n\nreleaseDate='$releaseDate'\n\nimageURI=$imageURI)"
