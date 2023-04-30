1a985ba9fa19ae7901da9ea211f6028b76139fd8
Alexander <alexp0111@mail.ru>
2023-04-30 19:23:16 +0300

    Actual backend

--- app/src/main/java/com/ikbo0621/anitree/MainActivity.kt
+++ app/src/main/java/com/ikbo0621/anitree/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Keep this code
+        // Actual
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container_view, LogInFragment())
