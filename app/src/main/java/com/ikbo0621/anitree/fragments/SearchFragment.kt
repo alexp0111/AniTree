c53f45711e05bea4355a63a6b4136d894e484330
Bulat <konnor1500@icloud.com>
2023-05-02 12:59:12 +0300

    Almost made design for SearchFragment.kt

--- app/src/main/java/com/ikbo0621/anitree/fragments/SearchFragment.kt
+++ app/src/main/java/com/ikbo0621/anitree/fragments/SearchFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.ikbo0621.anitree.R
 import com.ikbo0621.anitree.databinding.FragmentSearchBinding
 
 
@@ -14,22 +16,27 @@ class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding ? = null
 
-    private val binding get() = _binding!!
+   private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
-        return binding.root
+       return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.searchButton.setOnClickListener {
-            val action = SearchFragmentDirections.actionSearchFragmentToAccountFragment()
-            Navigation.findNavController(view).navigate(action)
+        val animeList = arrayOf("Here","Must","Be","Anime","List")
 
-        }
+        val arrayAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,animeList)
+        binding.resultList.adapter = arrayAdapter
+        //binding.avatar.setOnClickListener {
+        //    val action = SearchFragmentDirections.actionSearchFragmentToAccountFragment()
+        //    Navigation.findNavController(requireView()).navigate(action)
+      //  }
+
     }
 
 
