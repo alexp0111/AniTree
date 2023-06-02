d78cee81e56adc5c0e355e423370adad46b48e98
MatveyMelnikov <matveyy-melnikov@rambler.ru>
2023-06-03 01:48:54 +0300

    Added DimensController initialization in MainActivity

--- app/src/main/java/com/ikbo0621/anitree/MainActivity.kt
+++ app/src/main/java/com/ikbo0621/anitree/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.ikbo0621.anitree
 
+import android.graphics.Point
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import com.ikbo0621.anitree.fragments.LogInFragment
+import com.ikbo0621.anitree.data.DimensController
+import com.ikbo0621.anitree.data.Layout
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -16,6 +18,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DimensController.init(
+            applicationContext,
+            Layout(),
+            Point(
+                resources.displayMetrics.widthPixels,
+                resources.displayMetrics.heightPixels
+            )
+        )
 
         // Keep this code
         // Actual
